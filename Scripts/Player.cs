a38f2e82df4060445d09876cf8b5e98d6356b454
N1NNIE <94722151+N1NNIE@users.noreply.github.com>
2022-03-06 12:02:03 -0600

    Add files via upload

---
+++ Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public OVRPlayerController ovrPlayer;
+    public bool godMode = false;
+    public float health = 100;
+    public float damage = 0;
+    public GameObject[] enemies;
+
+    void Start()
+    {
+        
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void EnableGodMode()
+    {
+        godMode = true;
+    }
+
+    public void DisableGodMode()
+    {
+        godMode = false;
+    }
+
+    public void KillAll()
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            Destroy(enemies[i]);
+        }
+    }
+}
